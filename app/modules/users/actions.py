51dee919ac00e80759dfde0ffae629c73b859765
Adrian Letchford <me@dradrian.com>
2015-04-18 08:44:24 +0100

    cleaned users.actions

--- app/modules/users/actions.py
+++ app/modules/users/actions.py
@@ -8,7 +8,7 @@ Author URL: http:www.DrAdrian.com
 
 This file defines the actions for the user module.
 """
-import endpoints
+from endpoints import UnauthorizedException, NotFoundException
 from google.appengine.ext import ndb
 import users.models as models
 
@@ -24,7 +24,7 @@ def verify_and_get_user(**kwargs):
 	user = models.User.get_from_dumb_token(kwargs['token'])
 
 	if user is None:
-		raise endpoints.UnauthorizedException("User is invalid.")
+		raise UnauthorizedException("User is invalid.")
 
 	return user
 
@@ -40,44 +40,46 @@ def user_key_id_to_user(uinput):
 	if type(uinput) is ndb.Key:
 		user = uinput.get()
 		if user is None:
-			raise endpoints.NotFoundException()
+			raise NotFoundException()
 		return user
 
 	if type(uinput) is long or type(uinput) is str:
 		user = models.User.get_by_id(uinput)
 		if user is None:
-			raise endpoints.NotFoundException()
+			raise NotFoundException()
 		return user
 
 	raise TypeError("Unknown type.")
 
 @ndb.transactional(xg=True)
-def _add_to_friends_list(userA, userB):
+def _add_to_friends_list(user_a, user_b):
+	"""Adds two users to eachother's friends list."""
 
-	if userA.key is None or userB.key is None:
-		raise endpoints.NotFoundException("Users do not exist.")
+	if user_a.key is None or user_b.key is None:
+		raise NotFoundException("Users do not exist.")
 
 	# TODO: Add to friends lists
-	friend_listA = models.FriendList.get_or_create_addable_friend_list(userA)
-	friend_listA.add_friend(userB.key)
+	friend_list_a = models.FriendList.get_or_create_addable_friend_list(user_a)
+	friend_list_a.add_friend(user_b.key)
 
-	friend_listB = models.FriendList.get_or_create_addable_friend_list(userB)
-	friend_listB.add_friend(userA.key)
+	friend_list_b = models.FriendList.get_or_create_addable_friend_list(user_b)
+	friend_list_b.add_friend(user_a.key)
 
-	friend_listA.put()
-	friend_listB.put()
+	friend_list_a.put()
+	friend_list_b.put()
 
 
 @ndb.transactional(xg=True)
-def _remove_from_friends_list(userA, userB):
+def _remove_from_friends_list(user_a, user_b):
+	"""Removes two users from eachother's friends list."""
 
-	if userA.key is None or userB.key is None:
-		raise endpoints.NotFoundException("Users do not exist.")
+	if user_a.key is None or user_b.key is None:
+		raise NotFoundException("Users do not exist.")
 
-	users =[userA, userB]
+	users = [user_a, user_b]
 	for i in range(2):
 		current_user = users[i]
-		friend = users[(i+1)%2] 
+		friend = users[(i+1)%2]
 
 		query = models.FriendList.query(ancestor=current_user.key)
 		friend_list = query.get()
@@ -88,12 +90,14 @@ def _remove_from_friends_list(userA, userB):
 
 @ndb.transactional(xg=True)
 def friend_request(sender_id, receiever_id):
+	"""Send a friend request from a sending user to a receiving user."""
 
 	sender = models.User.get_by_id(sender_id)
 	reciever = models.User.get_by_id(receiever_id)
 
-	if sender is None or type(sender) is not models.User or reciever is None or type(reciever) is not models.User:
-		raise endpoints.NotFoundException("Users do not exist.")
+	if sender is None or type(sender) is not models.User or \
+		reciever is None or type(reciever) is not models.User:
+		raise NotFoundException("Users do not exist.")
 
 	# Get a relationship model in the transaction
 	relationship = models.Relationship.get_by_users(sender, reciever)
@@ -106,7 +110,9 @@ def friend_request(sender_id, receiever_id):
 		return relationship
 
 	# If the other person already sent a friend request
-	if relationship.friend_request_sent and relationship.friend_request_sender == reciever.key and relationship.friend_request_rejected == False:
+	if relationship.friend_request_sent and \
+			relationship.friend_request_sender == reciever.key and \
+			relationship.friend_request_rejected == False:
 		relationship.is_friends = True
 		_add_to_friends_list(reciever, sender)
 
@@ -122,27 +128,30 @@ def friend_request(sender_id, receiever_id):
 	return relationship
 
 @ndb.transactional(xg=True)
-def respond_to_friend_request(me, sender_id, accept):
-
+def respond_to_friend_request(auth_user, sender_id, accept):
+	"""
+	Send a response to a friend request sent by a sending user to the
+	authenticating user.
+	"""
 	sender = models.User.get_by_id(sender_id)
-	me = user_key_id_to_user(me)
+	auth_user = user_key_id_to_user(auth_user)
 
-	if sender is None or me is None:
-		raise endpoints.NotFoundException("Users do not exist.")
+	if sender is None or auth_user is None:
+		raise NotFoundException("Users do not exist.")
 
 	# Get a relationship model in the transaction
-	relationship = models.Relationship.get_by_users(sender, me)
+	relationship = models.Relationship.get_by_users(sender, auth_user)
 
 	if relationship is None or \
 			relationship.is_friends or \
 			not relationship.friend_request_sent or \
-			relationship.friend_request_sender == me.key:
-		raise endpoints.NotFoundException("This person did not send a friend request.")
+			relationship.friend_request_sender == auth_user.key:
+		raise NotFoundException("This person did not send a friend request.")
 
 	if accept:
 		relationship.is_friends = True
 		relationship.friend_request_rejected = False
-		_add_to_friends_list(me, sender)
+		_add_to_friends_list(auth_user, sender)
 		# TODO: create notification
 	else:
 		relationship.friend_request_rejected = True
@@ -152,47 +161,50 @@ def respond_to_friend_request(me, sender_id, accept):
 	return relationship
 
 @ndb.transactional(xg=True)
-def unfriend(me_id, sender_id):
+def unfriend(unfriender_id, friend_id):
+	"""Remove the user as a friend."""
 
-	sender = models.User.get_by_id(sender_id)
-	me = models.User.get_by_id(me_id)
+	friend = models.User.get_by_id(friend_id)
+	unfriender = models.User.get_by_id(unfriender_id)
 
-	if sender is None or me is None:
-		raise endpoints.NotFoundException("Users do not exist.")
+	if friend is None or unfriender is None:
+		raise NotFoundException("Users do not exist.")
 
 	# Get a relationship model in the transaction
-	relationship = models.Relationship.get_by_users(sender, me)
+	relationship = models.Relationship.get_by_users(friend, unfriender)
 
 	if relationship is None:
-		relationship = models.Relationship.create(sender, me)
+		relationship = models.Relationship.create(friend, unfriender)
 
 	relationship.is_friends = False
-	relationship.friend_unfriender = me.key
+	relationship.friend_unfriender = unfriender.key
 
-	_remove_from_friends_list(sender, me)
+	_remove_from_friends_list(friend, unfriender)
 
 	relationship.put()
 
 	return relationship
 
 
-def get_relationship(userA_input, userB_input):
+def get_relationship(user_a_, user_b_):
+	"""Returns the relationship between two users."""
 
-	userA = user_key_id_to_user(userA_input)
-	userB = user_key_id_to_user(userB_input)
+	user_a = user_key_id_to_user(user_a_)
+	user_b = user_key_id_to_user(user_b_)
 
-	if userA is None or userB is None:
-		raise endpoints.NotFoundException("Users do not exist.")
+	if user_a is None or user_b is None:
+		raise NotFoundException("Users do not exist.")
 
-	relation = models.Relationship.get_by_users(userA, userB)
+	relation = models.Relationship.get_by_users(user_a, user_b)
 
 	if relation is None:
-		relation = models.Relationship.create(userA, userB)
+		relation = models.Relationship.create(user_a, user_b)
 
 	return relation
 
 
 def get_friends_list(user):
+	"""Returns the list of friends of a user."""
 	# TODO Paginate
 
 	user = user_key_id_to_user(user)
