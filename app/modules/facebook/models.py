3509288edd0c853e4e29f197c708a325237b95b3
Adrian Letchford <me@dradrian.com>
2015-04-18 09:13:51 +0100

    cleaned facebook.models

--- app/modules/facebook/models.py
+++ app/modules/facebook/models.py
@@ -14,33 +14,35 @@ from users.models import User
 
 
 class FacebookAccount(ndb.Model):
-    """
-    Defines a Facebook account with credentials.
+	"""
+	Defines a Facebook account with credentials.
 
-    Parent: User
+	Parent: User
 
-    Fields:
-        tokensecret - A single index of both the token and secret. This enables
-            us to search by token and secret without requiring two separate
-            indexes.
-    """
+	Fields:
+		tokensecret - A single index of both the token and secret. This enables
+			us to search by token and secret without requiring two separate
+			indexes.
+	"""
 
-    # Parent = User
-    facebook_id = ndb.IntegerProperty(indexed=True, required=True) 
-    access_token = ndb.StringProperty(indexed=True, required=True)
-    expires = ndb.DateTimeProperty(indexed=False, required=False)
+	# Parent = User
+	facebook_id = ndb.IntegerProperty(indexed=True, required=True)
+	access_token = ndb.StringProperty(indexed=True, required=True)
+	expires = ndb.DateTimeProperty(indexed=False, required=False)
 
-    def validate(self):
-        """Validates this model."""
-        misc.validate_parent(self, User)
+	def validate(self):
+		"""Validates this model."""
+		misc.validate_parent(self, User)
 
-    def put(self):
-        """Overrides the default put() behaviour to setup automatic fields."""
-        self.validate()
-        super(FacebookAccount, self).put()
+	def put(self):
+		"""Overrides the default put() behaviour to setup automatic fields."""
+		self.validate()
+		super(FacebookAccount, self).put()
 
-    @staticmethod
-    def get_by_facebook_id(twitter_id):
-        q = FacebookAccount.query(FacebookAccount.facebook_id == long(twitter_id))
-        facebook_id = q.get()
-        return facebook_id    
+	@staticmethod
+	def get_by_facebook_id(facebook_id):
+		"""Returns a FacebookAccount from a Facebook user ID."""
+		query = FacebookAccount.query(
+			FacebookAccount.facebook_id == long(facebook_id))
+		account = query.get()
+		return account    
