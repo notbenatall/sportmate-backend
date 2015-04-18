86a2c38f6072fcff483635eeb57a4a7cd1d69d87
Adrian Letchford <me@dradrian.com>
2015-04-18 09:03:46 +0100

    cleaned facebook.messages

--- app/modules/facebook/messages.py
+++ app/modules/facebook/messages.py
@@ -6,6 +6,7 @@ Module: facebook
 Author: Dr. Adrian Letchford
 Author URL: http:www.DrAdrian.com
 """
+# pylint: disable=too-few-public-methods
 
 from protorpc import messages
 from protorpc import message_types
@@ -13,13 +14,19 @@ from protorpc import message_types
 import users.messages
 
 
-class UrlMessage(messages.Message):
-    url = messages.StringField(1)
+class Url(messages.Message):
+	"""Message containing a URL."""
+	url = messages.StringField(1)
 
-class CodeMessage(messages.Message):
-    code = messages.StringField(1)
+class Code(messages.Message):
+	"""Message containing Facebook's client authentication code."""
+	code = messages.StringField(1)
 
 class FacebookAccountWithUser(messages.Message):
+	"""
+	Message containing a Facebook account with the corresponding
+	Sportmate user.
+	"""
 	facebook_id = messages.IntegerField(1)
 	access_token = messages.StringField(2)
 	expires = message_types.DateTimeField(3)
